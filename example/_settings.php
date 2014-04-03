890171a2bcb3b6284d0ad1b76495b336a32c6261
Merijn Bertels <merijn.bertels@gmail.com>
2014-04-03 21:14:40 +0200

    add new code

---
+++ example/_settings.php
@@ -0,0 +1,8 @@
+<?php
+	
+$SERVER = '';
+$USERNAME = '';
+$PASSWORD = '';
+$CHANNEL = '';
+		
+?>
--- example/client.php
+++ example/client.php
@@ -1,11 +1,7 @@
 <?php
 
+require('settings.php');
 require('../client.php');
-
-$SERVER = '';
-$USERNAME = '';
-$PASSWORD = '';
-$CHANNEL = '';
 
 $client = new TradeApiClient($SERVER, $USERNAME, $PASSWORD, $CHANNEL);
 
